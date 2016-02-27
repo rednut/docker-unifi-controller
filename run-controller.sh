e265f8685708e0a45620938137076098a8b4470c
Stuart Nixon <stuart@rednut.net>
2016-02-27 00:40:54 +0000

    tidy run-controller.sh

--- run-controller.sh
+++ run-controller.sh
@@ -12,6 +12,9 @@ docker rm -f $CONTAINER_NAME || echo "not a container yet: '$CONTAINER_NAME'"
 
 
 docker run -d \
-	-p 2222:22 -p 8080:8080 -p 8443:8443 -p 37117:27117 \
-                        -v /srv/data/apps/docker/unifi/data:/usr/lib/unifi/data \
+	-p 2222:22 \
+	-p 8081:8080 \
+	-p 8443:8443 \
+	-p 37117:27117 \
+                        -v /docker/unifi/data:/usr/lib/unifi/data \
                         --name unifi rednut/unifi-controller
